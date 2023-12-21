ee8e784b72c142809a205228b2f03cd4b09afbd4
Jasand <smileonyourface@yandex.com>
2023-12-21 09:47:05 +0500

    Task 2

---
+++ 02/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2. Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные
+
+Console.Write("Введите предложение: ");
+string sentence = Console.ReadLine()!;
+sentence = sentence.ToLower();
+Console.WriteLine(sentence);
