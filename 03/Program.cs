dacb302e3ee42cd41cff82b28d46a4cacfff142d
Jasand <smileonyourface@yandex.com>
2023-12-21 10:09:21 +0500

    Task 3

---
+++ 03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3. Задайте произвольную строку
+// Выясните, является ли она палиндромом
+
+bool palindrom(string str)
+{
+    string check = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return check.SequenceEqual(check.Reverse());
+}
+
+Console.Write("Введите предложение: ");
+string sentence = Console.ReadLine()!;
+bool ifitis = palindrom(sentence);
+Console.WriteLine(ifitis ? "Да" : "Нет");
